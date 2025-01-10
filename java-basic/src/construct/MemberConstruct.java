3e4e63a87fedfc0139fe37031333d46e071d6fa6
jungil0617 <jungil0617@naver.com>
2025-01-10 21:30:07 +0900

    #1 생성자

---
+++ java-basic/src/construct/MemberConstruct.java
@@ -0,0 +1,14 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age, int grade) {
+        System.out.println("생성자 호출 이름 : " + name + " 나이 : " + age + " 성적 : " + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
